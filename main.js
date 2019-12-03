9bc2aa04c1caa48d8b92182bec4183d4eeaf3fb9
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2019-12-03 15:26:36 +0100

    complete commit

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const inputs = document.querySelectorAll('.controls input');
 //console.log(inputs);
 
 function handleupdate(){
-    console.log(this.value);
+    //console.log(this.dataset.data-sizing);
+    const suffix = this.dataset.sizing || " ";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix );
 }
 
 inputs.forEach(input => input.addEventListener('change', handleupdate));
